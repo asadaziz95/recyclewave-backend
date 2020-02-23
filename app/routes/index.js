41b7afc9ca14711b27e5d4837995aafb2bf20cfa
asadaziz <asadazizjs@gmail.com>
2020-02-24 02:07:01 +0500

    routes added

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 // this is the main file for all routes
-
+const login = require('./login');
 const createorder = require('./createandgetsorders');
 
 module.exports = function(app,database)
 {
     
     // signupRoute(app,database);
-    // loginRoute(app,database);
+     login(app,database);
     // logoutRoute(app,database);
     // updateRoute(app,database);
     // createandgetstours(app,database);
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,16 +1,14 @@
 const _ = require('lodash');
-const {Traveller} = require('./../models/travellersignup');
-const {TravellingAgency}=require('./../models/travellingagencysignup')
-const {PropertyLender} = require('./../models/propertylendersignup');
+const {User} = require('./../models/User');
 
 
 module.exports=function(app,database)
 {
 
-app.post('/travellerlogin',(req,res)=>{
+app.post('/login',(req,res)=>{
     var body = _.pick(req.body, ['email', 'password']);
     
-    Traveller.findByCredentials(body.email, body.password).then((user) => {
+    User.findByCredentials(body.email, body.password).then((user) => {
         return user.generateAuthToken().then((token) => {
         res.status(200).send({'data':user,'token':token});
       });
@@ -19,29 +17,5 @@ app.post('/travellerlogin',(req,res)=>{
     });
 })
 
-app.post('/travellingagencylogin',(req,res)=>{
-    var body  = _.pick(req.body,['email','password']);
-    TravellingAgency.findByCredentials(body.email,body.password).then((user)=>{
-       return user.generateAuthToken().then((token)=>{
-        res.status(200).send({'data':user,'token':token});
-       })
-    }).catch((e)=>{
-        res.status(400).send(e);
-    })  
-})
-
-app.post('/propertylenderlogin',(req,res)=>{
-    console.log('prop')
-    var body  = _.pick(req.body,['email','password']);
-    PropertyLender.findByCredentials(body.email,body.password).then((user)=>{
-       return user.generateAuthToken().then((token)=>{
-           
-        res.status(200).send({'data':user,'token':token});
-       })
-    }).catch((e)=>{
-        console.log(e)
-        res.status(400).send(e);
-    })  
-})
 
 }
